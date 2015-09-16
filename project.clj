cb4c08d204e5e4c576736ba1d0f8e67658a680eb
bago2k4 <iacopo@rc0.eu>
2015-09-16 13:36:33 +0200

    use secretary 2 and update package version

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject venantius/accountant "0.1.1"
+(defproject venantius/accountant "0.2-SNAPSHOT"
   :description "Navigation for Single-Page Applications Made Easy."
   :url "http://github.com/venantius/accountant"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.48"]
-                 [secretary "1.2.3"]])
+                 [secretary "2.0.0.1-260a59"]])
