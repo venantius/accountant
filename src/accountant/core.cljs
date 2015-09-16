3f6bd830b975a3d6800783ef24b18dd627998b81
bago2k4 <iacopo@rc0.eu>
2015-09-16 13:38:54 +0200

    initialize accountant passing in a dispatcher and a route locator, the first is used to dispatch a route when needed the second is used to check if the next token should be handled with secretary or with a page reload. also improve the check on the link clicked: if the domain is different or the target is different then the current or the path is not handled by secretary reload the page

--- src/accountant/core.cljs
+++ src/accountant/core.cljs
@@ -15,44 +15,52 @@
     out))
 
 (defn- dispatch-on-navigate
-  [history]
+  [history secretary-dispatcher]
   (let [navigation (listen history EventType/NAVIGATE)]
     (go
       (while true
         (let [token (.-token (<! navigation))]
-          (secretary/dispatch! token))))))
+          (secretary-dispatcher token))))))
 
-(defn- find-href
+(defn- find-a-attrs
   "Given a DOM element that may or may not be a link, traverse up the DOM tree
-  to see if any of its parents are links. If so, return the href content."
+  to see if any of its parents are links. If so, return the href and the target content."
   [e]
-  ((fn [e]
-     (if-let [href (.-href e)]
-        href
-        (when-let [parent (.-parentNode e)]
+  ((fn [el]
+     (if-let [href (.-href el)
+              target (.-target el)]
+        [href target]
+        (when-let [parent (.-parentNode el)]
            (recur parent)))) (.-target e)))
 
 (defn- prevent-reload-on-known-path
   "Create a click handler that blocks page reloads for known routes in
   Secretary."
-  [history]
+  [history route-locator]
   (events/listen
    js/document
    "click"
    (fn [e]
-     (let [href (find-href e)
-           path (.getPath (.parse Uri href))
+     (let [[href target] (find-a-attrs)
+           parsed-uri (.parse Uri href)
+           path (.getPath parsed-uri)
+           domain (.getDomain parsed-uri)
            title (.-title (.-target e))]
-       (when (secretary/locate-route path)
+       (when (and (or (= domain "")                    ; the domain is empty
+                      (= domain (.. js/window -location -hostname))) ; or is the same of the current domain
+                  (or (nil? target)                    ; the target of the a tag is empty
+                      (= target "")                    ;   "     "     "     "
+                      (= target "_self"))              ; or is _self
+                  (route-locator path))                ; path is handled by secretary
          (. history (setToken path title))
          (.preventDefault e))))))
 
 (defn configure-navigation!
   "Create and configure HTML5 history navigation."
-  []
+  [secretary-dispatcher route-locator]
   (let [history (Html5History.)]
     (.setUseFragment history false)
     (.setPathPrefix history "")
     (.setEnabled history true)
-    (dispatch-on-navigate history)
-    (prevent-reload-on-known-path history)))
+    (dispatch-on-navigate history secretary-dispatcher)
+    (prevent-reload-on-known-path history route-locator)))
