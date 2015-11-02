851cd2a4c9f2a3aa0d82f9f167e3128659378d49
bago2k4 <iacopo@rc0.eu>
2015-11-02 22:34:05 +0100

    use a custom build-trasformer function to setup Html5Hisory object, add locate-route function to check if secretary handles the current token, bug fix

--- src/accountant/core.cljs
+++ src/accountant/core.cljs
@@ -27,40 +27,66 @@
   to see if any of its parents are links. If so, return the href and the target content."
   [e]
   ((fn [el]
-     (if-let [href (.-href el)
-              target (.-target el)]
-        [href target]
-        (when-let [parent (.-parentNode el)]
-           (recur parent)))) (.-target e)))
+     (if-let [href (.-href el)]
+       (if-let [target (.-target el)]
+          [href target]
+          (when-let [parent (.-parentNode el)]
+             (recur parent)))
+       (when-let [parent (.-parentNode el)]
+             (recur parent)))) (.-target e)))
+
+(defn- locate-route [routes needle]
+  (some
+   (fn [route]
+     (when (secretary/route-matches route needle)
+       route))
+   routes))
 
 (defn- prevent-reload-on-known-path
   "Create a click handler that blocks page reloads for known routes in
   Secretary."
-  [history route-locator]
+  [history routes]
   (events/listen
    js/document
    "click"
    (fn [e]
-     (let [[href target] (find-a-attrs)
+     (let [[href target] (find-a-attrs e)
            parsed-uri (.parse Uri href)
            path (.getPath parsed-uri)
            domain (.getDomain parsed-uri)
            title (.-title (.-target e))]
        (when (and (or (= domain "")                    ; the domain is empty
                       (= domain (.. js/window -location -hostname))) ; or is the same of the current domain
-                  (or (nil? target)                    ; the target of the a tag is empty
-                      (= target "")                    ;   "     "     "     "
+                  (or (empty? target)                  ; the target of the a tag is empty
                       (= target "_self"))              ; or is _self
-                  (route-locator path))                ; path is handled by secretary
+                  (locate-route routes path))          ; path is handled by secretary
          (. history (setToken path title))
          (.preventDefault e))))))
 
+; this is needed as of this
+; https://code.google.com/p/closure-library/source/detail?spec=svn88dc096badf091f380b4c2b4a6514184511de657&r=88dc096badf091f380b4c2b4a6514184511de657
+; setToken doen't replace the query string, it only attach it at the end
+; solution here: https://github.com/Sparrho/supper/blob/master/src-cljs/supper/history.cljs
+(defn- build-transformer
+  "Custom transformer is needed to replace query parameters, rather
+  than adding to them.
+  See: https://gist.github.com/pleasetrythisathome/d1d9b1d74705b6771c20"
+  []
+  (let [transformer (goog.history.Html5History.TokenTransformer.)]
+    (set! (.. transformer -retrieveToken)
+          (fn [path-prefix location]
+            (str (.-pathname location) (.-search location))))
+    (set! (.. transformer -createUrl)
+          (fn [token path-prefix location]
+            (str path-prefix token)))
+    transformer))
+
 (defn configure-navigation!
   "Create and configure HTML5 history navigation."
-  [secretary-dispatcher route-locator]
-  (let [history (Html5History.)]
+  [secretary-dispatcher routes]
+  (let [history (goog.history.Html5History. js/window (build-transformer))]
     (.setUseFragment history false)
     (.setPathPrefix history "")
     (.setEnabled history true)
     (dispatch-on-navigate history secretary-dispatcher)
-    (prevent-reload-on-known-path history route-locator)))
+    (prevent-reload-on-known-path history routes)))
