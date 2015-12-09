2a8103c5089ef6afcdf3604bf70fc707b269b5ec
bago2k4 <iacopo@rc0.eu>
2015-12-09 23:01:48 +0100

    update all the dependencies to the latest available including secretary 2

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject venantius/accountant "0.1.6"
+(defproject venantius/accountant "0.2-SNAPSHOT"
   :description "Navigation for Single-Page Applications Made Easy."
   :url "http://github.com/venantius/accountant"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.48"]
-                 [secretary "1.2.3"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]])
+  :dependencies [[org.clojure/clojure "1.8.0-RC3"]
+                 [org.clojure/clojurescript "1.7.189"]
+                 [secretary "2.0.0.1-260a59"]
+                 [org.clojure/core.async "0.2.374"]])
